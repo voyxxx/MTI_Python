913d7e176517e79bb1f1a4cb745413265e0a1b48
araks <voyxxx@mail.ru>
2024-03-19 00:54:58 +0300

    Added verification of the allowed planet to travel to

--- Group6/CreatingBotTelegrams/10_overview lecture/project.py
+++ Group6/CreatingBotTelegrams/10_overview lecture/project.py
@@ -11,7 +11,7 @@ planets = {
         'species': 'призрачные спектры',
         'planet': 'туманность',
         'resource': 'редкий газ'
-        },
+    },
     '2': {
         'text': 'Бронированные Титаны - это мощные существа, обитающие на планете-гиганте **Титан-7**, покрытой мощной броней. Основной ресурс - металлы и минералы, используемые для производства оружия и брони. Их жадность и склонность к роскоши делает их уязвимыми для покупки. Слабость: Купить.',
         'species': 'бронированные титаны',
@@ -76,11 +76,11 @@ planets = {
 
 questions = {
     '1': 'С какой планеты вы прибыли?',
-    '2': 'Какой основной ресурс на планете?', # добавить в вопрос конкретную планету
-    '3': 'На какой планете обитает раса ?', # добавить в вопрос расу
+    '2': 'Какой основной ресурс на планете?',  # добавить в вопрос конкретную планету
+    '3': 'На какой планете обитает раса ?',  # добавить в вопрос расу
 }
 
-greeting = 'Вы готовы отправиться в путешествие к самой мудрой и всезнающей цивилизации во вселенной? Планета, населенная существами, которые собрали все знания и информацию во вселенной в огромные библиотеки, ожидает вас. Вы сможете обучаться у этих мудрых существ и получить доступ к несметным знаниям и мудрости.'
+greeting = 'Вы готовы отправиться в путешествие к самой мудрой и всезнающей цивилизации во вселенной? Планета, населенная существами, которые собрали все знания и информацию во вселенной в огромные библиотеки, ожидает вас. Вы сможете обучаться у этих мудрых существ и получить доступ к несметным знаниям и мудрости.\n Ваша цель добраться до планеты cапиенция'
 
 
 locations = {
@@ -98,7 +98,7 @@ locations = {
             '9': 15,
             '10': 6,
             '11': 16
-        },        
+        },
     },
     '2': {
         'text': '',
@@ -262,38 +262,164 @@ locations = {
     }
 }
 
+actions = {
+    'move': 'Отправиться на другую планету.',
+    'answer': 'Ответить на вопрос.',
+    'risk': 'Рискнуть своей жизнью. Выигрыш 500 монет.',
+    'buy': 'Купить топливо.',
+    'steal': 'Украсть топливо.',
+    'takeByForce': 'Отобрать топливо силой.',
+}
+
 game_locations = {}
+
 
 def getReward():
     return random.randint(1, 5)
 
+
 def getPenalty():
     return random.randint(1, 3)
+
 
 players = {
     'player1': {
         'name': 'Сильный',
         'power': 5,
-        'agilty': 0,
+        'agility': 0,
         'eloquence': 0
     },
     'player2': {
         'name': 'Ловкий',
         'power': 0,
-        'agilty': 5,
+        'agility': 5,
         'eloquence': 0
     },
     'player3': {
         'name': 'Красноречивый',
         'power': 0,
-        'agilty': 0,
+        'agility': 0,
         'eloquence': 5
     }
 }
 
 player = {}
 
-def adjustLocation():
+
+def endGame(chatId):
+    player[chatId]['gameIsBegin'] = False
+
+
+# Посмотреть список доступных для путешествия планет
+def checkAllowedPlanet(userId):
+    currentPlanetDistances = player[userId]['loc'][str(player[userId]['currentPlanetNumber'])]['distances']
+    fuel = player[userId]['fuel']
+    player[userId]['allowedDistances'] = {}
+    for distance in currentPlanetDistances:
+        if currentPlanetDistances[distance] <= fuel:
+            player[userId]['allowedDistances'].update({distance: currentPlanetDistances[distance]})
+
+
+def getCurGoldAndFuel(userId):
+    return f'доступно кол-во топлива: {player[userId]["fuel"]}, кол-во золота {player[userId]["gold"]}'
+
+
+def displayMoveVariants(chatId, userId):
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    text = ''
+    if len(player[userId]['allowedDistances']) >= 0:
+        for distance in player[userId]['allowedDistances']:
+            planet = player[userId]['loc'][distance]['planet']
+            keyboard.add(telebot.types.InlineKeyboardButton(f'{planet} - {player[userId]['allowedDistances'][distance]} топлива', callback_data=planet))
+        text = f'{getCurGoldAndFuel(userId)}. Вам доступны путешествия на следующие планеты:'
+    else:
+        text = f'Доступных планет нет, {getCurGoldAndFuel(userId)}.'
+    keyboard.add(telebot.types.InlineKeyboardButton('вернуться назад', callback_data='back'))
+    bot.edit_message_text(text, chatId, player[userId]['msgId'], reply_markup=keyboard)
+
+
+@bot.callback_query_handler(func=lambda call: call.data in actions)
+def handlerAction(call):
+    chatId = call.message.chat.id
+    userId = call.from_user.id
+    # Если игрок выбрал отправиться на другую планету
+    if call.data == 'move':
+        # Проверить список доступных планет
+        checkAllowedPlanet(userId)
+        # Показать список планет для передвижения
+        displayMoveVariants(chatId, userId)
+
+    elif call.data == 'answer':
+        print(1)
+    elif call.data == 'risk':
+        print(1)
+    elif call.data == 'buy':
+        bot.send_message(call.message.chat.id, 'Вы купили топливо')
+    elif call.data == 'steal':
+        bot.send_message(call.message.chat.id, 'Вы украли топливо')
+    elif call.data == 'takeByForce':
+        bot.send_message(call.message.chat.id, 'Вы отобрали топливо силой')
+
+
+def startTrip(chatId, userId):
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    for action in actions:
+        keyboard.add(telebot.types.InlineKeyboardButton(actions[action], callback_data=action))
+    textValues = f'Вы находитесь на планете {player[userId]["birthPlace"]["planet"]}, в вашем корабле:\n{getCurGoldAndFuel(userId)}.\n'
+    text = 'Ваши действия:'
+    msgBot = bot.send_message(chatId, textValues + text, reply_markup=keyboard)
+    player[userId]['msgId'] = msgBot.message_id
+
+
+# Пометка планеты посещённой
+def checkVisitedPlaner(chatId, planet):
+    global player
+    for location in player[chatId]['loc'].values():
+        if location['planet'] == planet:
+            location['isVisited'] = True
+
+
+# Помечаем текущую планету
+def checkCurrentPlanetNumber(chatId, num):
+    player[chatId]['currentPlanetNumber'] = num
+
+
+# Выбираем случайную расу и планету из списка
+def randomChoiceSpeciesAndBirthPlace(userId):
+    global player
+    # Выбираем случайное число от 1 до 8
+    birthPlanetNumber = random.randint(1, 8)
+    # Создаём переменную с случайной планетой
+    birthPlace = game_locations[userId][str(birthPlanetNumber)]
+    # Добавляем в место рождения номер планеты
+    birthPlace.update({'planetNumber': birthPlanetNumber})
+    # Присваиваем в место рождения игрока словарь с случайной планетой
+    player[userId]['birthPlace'] = birthPlace
+    # Пометка планеты рождения посещённой
+    checkVisitedPlaner(userId, birthPlace['planet'])
+    # Помечаем текущую планету
+    checkCurrentPlanetNumber(userId, birthPlanetNumber)
+
+
+# Обработка события выбора персонажа
+@bot.callback_query_handler(func=lambda call: call.data in players)
+def initPlayer(call):
+    chatId = call.message.chat.id
+    userId = call.from_user.id
+    global player
+    # Инициализация объекта player для конкретного пользователя
+    player[userId].update(deepcopy(players[call.data]))
+    player[userId].update({'loc': game_locations[userId], 'gold': 0, 'fuel': 3})
+    # Выбираем случайную расу и планету из списка
+    randomChoiceSpeciesAndBirthPlace(userId)
+    answer = f'Вы выбрали игрока {player[userId]['name']} - расы {player[userId]["birthPlace"]["species"]}.'
+    bot.send_message(chatId, answer)
+    # Начало путешествия
+    startTrip(chatId, userId)
+
+
+# Настраиваем стартовую локацию
+def adjustLocation(userId):
     global locations, planets, game_locations
     # Перемешиваем значения planets и присваиваем их в новую переменную
     temp_planets = list(deepcopy(planets).values())
@@ -301,39 +427,29 @@ def adjustLocation():
     random.shuffle(shuffledPlanets)
     shuffledPlanets += temp_planets[8:]
     # Объединяем перемешанные значения planets и locations в один словарь game_locations
-    game_locations = deepcopy(locations)
-    for key, loc in game_locations.items():
-        # if (int(key) > 0):
-        loc.update(shuffledPlanets[int(key)-1])
+    game_locations[userId] = deepcopy(locations)
+    for key, loc in game_locations[userId].items():
+        loc.update(shuffledPlanets[int(key) - 1])
 
-def checkVisitePlaner(id, planet):
-    global player
-    for location in player[id]['loc'].values():
-        if (location['planet'] ==  planet):
-            location['isVisited'] = True
 
-# Выбираем случайную расу и планету из списка
-def randomChoiceSpeciesAndBirthPlace(id):
-    global player
-    birthPlace = deepcopy(random.choice(list(planets.items())[0:8])[1])
-    player[id]['birthPlace'] = birthPlace
-    checkVisitePlaner(id, birthPlace['planet'])
-
-def startGame(id):
+# Команда для начала игры
+@bot.message_handler(commands=['game'])
+def game(msg):
+    chatId = msg.chat.id
+    userId = msg.from_user.id
     # Проверяем начинал ли пользователь игру
-    if ((id in player) and ('gameIsBegin' in player[id])):
+    if (userId in player) and ('gameIsBegin' in player[userId]):
         # Если игра начата прекращаем запуск новой игры
-        if (player[id]['gameIsBegin'] == True):
-            bot.send_message(id, 'игра уже начата')
+        if player[userId]['gameIsBegin']:
+            bot.send_message(chatId, 'игра уже начата')
             return
-    # Проверяем начинал ли пользователь игру
     else:
-        player[id] = {
+        player[userId] = {
             'gameIsBegin': True
         }
-        
-    player[id]['gameIsBegin'] = True
-    adjustLocation()
+    player[userId]['gameIsBegin'] = True
+    # Настраиваем стартовую локацию
+    adjustLocation(userId)
     # Создаем клавиатуру
     keyboard = telebot.types.InlineKeyboardMarkup()
     # Создаем кнопки для выбора аватара
@@ -342,42 +458,14 @@ def startGame(id):
         button.append(telebot.types.InlineKeyboardButton(players[i]['name'], callback_data=i))
     keyboard.add(*button)
     # Отправляем сообщение с кнопками
-    bot.send_message(id, f'{greeting} \nВыберите аватара:', reply_markup=keyboard)        
+    bot.send_message(chatId, f'{greeting} \nВыберите аватара:', reply_markup=keyboard)
 
-def endGame(id): 
-  player[id]['gameIsBegin'] = False
 
 # Команда для старта бота
 @bot.message_handler(commands=['start'])
 def start(msg):
-    keyboard = telebot.types.ReplyKeyboardMarkup(True)
-    keyboard.add('Начать игру')
-    bot.send_message(msg.chat.id, f'Привет! Если хочешь сыграть, нажми на кнопку "Начать игру"', reply_markup=keyboard)
-
-# Команда для начала игры
-@bot.message_handler(commands=['game'])
-def game(msg):
-    startGame(msg.chat.id)
-
-# TODO удалить в финале
-@bot.message_handler(func = lambda x: x.text == 'Начать игру')
-def game(msg):
-    keyboard_remove = telebot.types.ReplyKeyboardRemove()
-    bot.send_message(msg.chat.id, 'Удаляем текущую клавиатуру', reply_markup=keyboard_remove)
-    startGame(msg)
-
-@bot.callback_query_handler(func=lambda call: call.data in players)
-def initPlayer(call):
-    id = call.message.chat.id
-    global player
-    player[id].update(deepcopy(players[call.data]))
-    player[id].update({'loc': game_locations, 'gold': 0, 'fuel': 3})
-    randomChoiceSpeciesAndBirthPlace(id)
-    answer = f'Вы принадлежите к расе {player[id]["birthPlace"]["species"]} и находитесь планете {player[id]["birthPlace"]["planet"]}'
-    bot.send_message(call.message.chat.id, answer)
-    print(player)
+    bot.send_message(msg.chat.id, f'Привет! Если хочешь сыграть, введи команду /game')
 
 
 if __name__ == '__main__':
     bot.infinity_polling()
- 
