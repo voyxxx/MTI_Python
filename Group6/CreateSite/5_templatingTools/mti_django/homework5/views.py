1538aae04dfbf86fb5f150dc0c9a859ac351c2ed
V_Belozerov <VB@Air13.local>
2024-04-10 13:09:05 +0300

    module 4 fix naming

--- Group6/CreateSite/5_templatingTools/mti_django/homework5/views.py
+++ Group6/CreateSite/5_templatingTools/mti_django/homework5/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template.response import TemplateResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse('jdslkfadjlk')
